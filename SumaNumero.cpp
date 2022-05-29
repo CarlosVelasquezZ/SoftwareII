81a3379bd73198d16125de7d1d6981e8972e3df7
SergioNaranjo100 <106038445+SergioNaranjo100@users.noreply.github.com>
2022-05-29 16:39:13 -0500

    Actualizacion Sergio VS

--- SumaNumero.cpp
+++ SumaNumero.cpp
@@ -3,21 +3,21 @@
 
 int main()
 {
-	/*Comentario Sergio Naranjo*/
+	/*Comentario Sergio Naranjo VS*/
     int n1, n2, producto, suma;
 
     printf( "\n   Introduzca primer numero (entero): " );
     scanf( "%d", &n1 );
     printf( "\n   Introduzca segundo numero (entero): " );
     scanf( "%d", &n2 );
-    /*Comentario Wilmer Jiménez*/
+    /*Comentario Wilmer Jimï¿½nez*/
 
     suma = n1 + n2;
     producto = n1 * n2;
 
     printf( "\n   La suma es: %d", suma );
     printf( "\n\n   La multiplicaci%cn es: %d", 162, producto );
-	/*Comentario Vverónica Toasa*/
+	/*Comentario Vverï¿½nica Toasa*/
 
     getch(); /* Pausa */
 
